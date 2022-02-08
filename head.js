7a3ef632d22c2216562af3c75df6578b4abedaa3
lalalalyt <lynneli318@gmail.com>
2022-02-08 17:04:12 -0500

    test files of functions assertEqual and head

--- head.js
+++ head.js
@@ -1,19 +1,8 @@
+const assertEqual = require ('./assertEqual')
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  let text;
-  if (actual === expected) {
-    text = "💚💚💚Assertion Passed: " + actual + " === " + expected;
-    console.log(text);
-  } else {
-    text = "🧡🧡🧡Assertion Failed: " + actual + " !== " + expected;
-    console.log(text);
-  }
-};
 
 const head = (input) => {
     return input[0]
 }
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head
