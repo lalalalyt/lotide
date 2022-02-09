d92155e54cabc35424e804cfbc16f65086c069a5
lalalalyt <lynneli318@gmail.com>
2022-02-08 19:59:17 -0500

    test file for the function middle

--- middle.js
+++ middle.js
@@ -1,27 +1,4 @@
-// test functions
-const assertArraysEqual = function (array1, array2) {
-  let text;
-  if (eqArrays(array1, array2)) {
-    text = "💚💚💚Assertion Passed";
-    console.log(text);
-  } else {
-    text = "🧡🧡🧡Assertion Failed";
-    console.log(text);
-  }
-};
 
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 //actual function
 const middle = (array) => {
@@ -36,11 +13,5 @@ const middle = (array) => {
   }
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); 
-assertArraysEqual(middle([1,2]), []); 
-assertArraysEqual(middle([1, 2, 3]), [2]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+module.exports= middle
 
